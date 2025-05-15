1782ad9be75f45ca79fd06f0c414fa4ef575a5af
unknown <20223020084@estudantes.ifpr.edu.br>
2025-05-15 11:09:32 -0300

    Mensagem do que foi feito

--- paginas/urls.py
+++ paginas/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path
 from .views import IndexView, SobreView
-from .views import CampusCreate, CategoriaCreate, UsuarioCreate
+from .views import CampusCreate, CategoriaCreate, UsuarioCreate, SugestaoCreate, ComentarioCreate, CursoCreate, TipoSolicitacaoCreate
 
+from .views import CampusUpdate, CategoriaUpdate, UsuarioUpdate, SugestaoUpdate, ComentarioUpdate, CursoUpdate, TipoSolicitacaoUpdate
 urlpatterns = [
 
     path("", IndexView.as_view(), name = "index"),
@@ -11,5 +12,17 @@ urlpatterns = [
     path("adicionar/campus/", CampusCreate.as_view(), name="inserir-campus"),
     path("adicionar/categoria/", CategoriaCreate.as_view(), name="inserir-categoria"),
     path("adicionar/usuario/", UsuarioCreate.as_view(), name="inserir-usuario"),
+    path("adicionar/sugestao/", SugestaoCreate.as_view(), name="inserir-sugestao"),
+    path("adicionar/comentario/", ComentarioCreate.as_view(), name="inserir-comentario"),
+    path("adicionar/curso/", CursoCreate.as_view(), name="inserir-curso"),
+    path("adicionar/tiposolicitacao/", TipoSolicitacaoCreate.as_view(), name="inserir-tiposolicitacao"),
 
+
+    path("editar/campus/<int:pk>/", CampusUpdate.as_view(), name="editar-campus"),
+    path("editar/categoria/<int:pk>/", CategoriaUpdate.as_view(), name="editar-categoria"),
+    path("editar/usuario/<int:pk>/", UsuarioUpdate.as_view(), name="editar-usuario"),
+    path("editar/sugestao/<int:pk>/", SugestaoUpdate.as_view(), name="editar-sugestao"),
+    path("editar/comentario/<int:pk>/", ComentarioUpdate.as_view(), name="editar-comentario"),
+    path("editar/curso/<int:pk>/", CursoUpdate.as_view(), name="editar-curso"),
+    path("editar/tiposolicitacao/<int:pk>/", TipoSolicitacaoUpdate.as_view(), name="editar-tiposolicitacao"),
 ]
--- paginas/views.py
+++ paginas/views.py
@@ -1,35 +1,112 @@
 from django.views.generic import TemplateView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-
-from .models import Campus, Categoria, Usuario
-
 from django.urls import reverse_lazy
 
+from .models import Campus, Categoria, Usuario, Sugestao, Comentario, Curso, TipoSolicitacao
+
+# Páginas estáticas
 class IndexView(TemplateView):
     template_name = "paginas/index.html"
 
 class SobreView(TemplateView):
     template_name = 'paginas/sobre.html'
 
+# CREATE VIEWS
 class CampusCreate(CreateView):
     template_name = 'paginas/form.html'
     model = Campus
     fields = ['nome']
     success_url = reverse_lazy('index')
-    extra_context = {'titulo' : 'Cadastro de Campus'}
+    extra_context = {'titulo': 'Cadastro de Campus'}
 
 class CategoriaCreate(CreateView):
     template_name = 'paginas/form.html'
     model = Categoria
     fields = ['nome']
     success_url = reverse_lazy('index')
-    extra_context = {'título' : 'Cadastro De Categoria'}
+    extra_context = {'titulo': 'Cadastro de Categoria'}
 
 class UsuarioCreate(CreateView):
     template_name = 'paginas/form.html'
     model = Usuario
     fields = ['nome', 'email', 'senha']
     success_url = reverse_lazy('index')
-    extra_context = {'título' : 'Cadastro de Usuário'}
+    extra_context = {'titulo': 'Cadastro de Usuário'}
 
+class SugestaoCreate(CreateView):
+    template_name = 'paginas/form.html'
+    model = Sugestao
+    fields = ['titulo', 'descricao', 'data_criacao', 'usuario', 'campus', 'categorias']
+    success_url = reverse_lazy('index')
+    extra_context = {'titulo': 'Cadastro de Sugestão'}
 
+class ComentarioCreate(CreateView):
+    template_name = 'paginas/form.html'
+    model = Comentario
+    fields = ['texto', 'data_comentario', 'usuario', 'sugestao']
+    success_url = reverse_lazy('index')
+    extra_context = {'titulo': 'Cadastro de Comentário'}
+
+class CursoCreate(CreateView):
+    template_name = 'paginas/form.html'
+    model = Curso
+    fields = ['nome', 'campus']
+    success_url = reverse_lazy('index')
+    extra_context = {'titulo': 'Cadastro de Curso'}
+
+class TipoSolicitacaoCreate(CreateView):
+    template_name = 'paginas/form.html'
+    model = TipoSolicitacao
+    fields = ['descricao', 'concluido']
+    success_url = reverse_lazy('index')
+    extra_context = {'titulo': 'Cadastro de Tipo de Solicitação'}
+
+# UPDATE VIEWS
+class CampusUpdate(UpdateView):
+    template_name = 'paginas/form.html'
+    model = Campus
+    fields = ['nome']
+    success_url = reverse_lazy('index')
+    extra_context = {'titulo': 'Atualização de Campus', 'botao': 'Salvar'}
+
+class CategoriaUpdate(UpdateView):
+    template_name = 'paginas/form.html'
+    model = Categoria
+    fields = ['nome']
+    success_url = reverse_lazy('index')
+    extra_context = {'titulo': 'Atualização de Categoria', 'botao': 'Salvar'}
+
+class UsuarioUpdate(UpdateView):
+    template_name = 'paginas/form.html'
+    model = Usuario
+    fields = ['nome', 'email', 'senha']
+    success_url = reverse_lazy('index')
+    extra_context = {'titulo': 'Atualização de Usuário', 'botao': 'Salvar'}
+
+class SugestaoUpdate(UpdateView):
+    template_name = 'paginas/form.html'
+    model = Sugestao
+    fields = ['titulo', 'descricao', 'data_criacao', 'usuario', 'campus', 'categorias']
+    success_url = reverse_lazy('index')
+    extra_context = {'titulo': 'Atualização de Sugestão', 'botao': 'Salvar'}
+
+class ComentarioUpdate(UpdateView):
+    template_name = 'paginas/form.html'
+    model = Comentario
+    fields = ['texto', 'data_comentario', 'usuario', 'sugestao']
+    success_url = reverse_lazy('index')
+    extra_context = {'titulo': 'Atualização de Comentário', 'botao': 'Salvar'}
+
+class CursoUpdate(UpdateView):
+    template_name = 'paginas/form.html'
+    model = Curso
+    fields = ['nome', 'campus']
+    success_url = reverse_lazy('index')
+    extra_context = {'titulo': 'Atualização de Curso', 'botao': 'Salvar'}
+
+class TipoSolicitacaoUpdate(UpdateView):
+    template_name = 'paginas/form.html'
+    model = TipoSolicitacao
+    fields = ['descricao', 'concluido']
+    success_url = reverse_lazy('index')
+    extra_context = {'titulo': 'Atualização de Tipo de Solicitação', 'botao': 'Salvar'}
