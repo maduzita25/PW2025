65bb723378f3d38624f86b6949b655a580eb0e9f
unknown <20223020084@estudantes.ifpr.edu.br>
2025-09-29 15:03:39 -0300

    preparação para o google cloud

--- projetopw/settings.py
+++ projetopw/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-
+STATIC_ROOT = 'static_gcloud/' # Alterar essa configuração
 STATICFILES_DIRS = [ BASE_DIR / "static", ]
 
 # Default primary key field type
